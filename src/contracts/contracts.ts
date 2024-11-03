236eff6feff8405ec3ad4a951743676cc2b2a2a0
Solomon Botchway <solomonbotchway7@gmail.com>
2024-11-03 17:01:36 +0000

    instantiate contracts

--- src/contracts/contracts.ts
+++ src/contracts/contracts.ts
@@ -1,6 +1,9 @@
 import { ethers } from "ethers";
+import * as config from "../config";
 import { Storage__factory } from "./types";
 
-const provider = new ethers.JsonRpcProvider(process.env.SEPOLIA_RPC_URL);
+const provider = new ethers.JsonRpcProvider(config.SEPOLIA_RPC_URL);
+const wallet = new ethers.Wallet(config.PRIVATE_KEY, provider);
 
 export const storageContract = Storage__factory.connect("0x4361c49A042Bb4730FA2590daa65B2804D26D093", provider);
+export const storageContractSigner = storageContract.connect(wallet);
